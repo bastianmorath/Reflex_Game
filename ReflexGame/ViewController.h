16135ae2f734dcde1485dc7f1e0248bc22421f8f
Bastian Morath <bastian.morath@gmx.ch>
2016-02-06 12:45:47 +0100

    INitial Commit

--- ReflexGame/ViewController.h
+++ ReflexGame/ViewController.h
@@ -9,5 +9,25 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+{
+    int score;
+    int buttonTagToPress;
+    NSTimer *timer;
+    NSTimer *countdownTimer;
+    int secondesLeft;
+    int tagToDelete;
+    UILabel *scoreLabel1;
+    UILabel *scoreLabel2;
+    UILabel *levelLabel;
+    int level;
+}
+- (IBAction)buttonPressed:(id)sender;
+
+@property (retain, nonatomic) IBOutletCollection(UIButton) NSMutableArray *buttons;
+@property (strong, nonatomic) IBOutlet UILabel *pointsLabel;
+
+@property (strong, nonatomic) IBOutlet UIButton *startButton;
+- (IBAction)startButtonPressed:(id)sender;
+@property (strong, nonatomic) IBOutlet UILabel *timeLabel;
 
 @end
--- ReflexGame/ViewController.m
+++ ReflexGame/ViewController.m
@@ -17,13 +17,152 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    buttonTagToPress = 0;
+    level=1;
+    tagToDelete=1;
+    secondesLeft = 10;
+
+    levelLabel = [[UILabel alloc]initWithFrame:CGRectMake(self.view.frame.size.width/2 - 260/2, 210, 260, 40)];
+    levelLabel.textColor=[UIColor whiteColor];
+    levelLabel.textAlignment=NSTextAlignmentCenter;
+    levelLabel.font=[UIFont fontWithName:Nil size:20];
+    [self.view addSubview:levelLabel];
+
+    self.timeLabel.text=[NSString stringWithFormat:@"%i", secondesLeft];
+    
+    /*Touch ändern*/
+    for (int i=1;i<35; i++) {
+        UIButton *button= (UIButton*)[self.view viewWithTag:i];
+        [button addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchDown];
+    }
+    
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)highliteNewButton{
+    [[self.view viewWithTag:buttonTagToPress] setBackgroundColor:[UIColor colorWithRed:101/255.0f green:228/255.0f blue:197/255.0f alpha:1]];
+    buttonTagToPress= arc4random() %35;
+
+    while (buttonTagToPress==0) {
+        buttonTagToPress= arc4random() %35;
+    }
+    UIButton *buttonToHighlite= (UIButton*)[self.view viewWithTag:buttonTagToPress];
+    
+    [buttonToHighlite setBackgroundColor:[UIColor colorWithRed:1 green:106/255.0f blue:62/255.0f alpha:1]];
+
 }
 
+
+-(void)updateScoreLabel{
+    self.pointsLabel.text= [NSString stringWithFormat:@"%i", score];
+}
+-(void)buttonPressed{
+        NSLog(@"%s", __PRETTY_FUNCTION__);
+
+}
+
+
+- (IBAction)startButtonPressed:(id)sender {
+    UIButton *button= sender;
+    buttonTagToPress = 1;
+
+    score=0;
+    self.pointsLabel.text= [NSString stringWithFormat:@"0"];
+
+    tagToDelete=1;
+    [scoreLabel1 setHidden:YES];
+    [scoreLabel2 setHidden:YES];
+    [levelLabel setHidden:YES];
+    /*Show all buttons ans Labels*/
+    [self.pointsLabel setHidden:NO];
+    self.timeLabel.text= @"10";
+
+    [self.timeLabel setHidden:NO];
+    
+    for (int i=1;i<=35; i++) {
+        [self.view viewWithTag:i].backgroundColor=[UIColor colorWithRed:101/255.0f green:228/255.0f blue:197/255.0f alpha:1];
+    }
+    button.alpha=0.3;
+    timer = [NSTimer scheduledTimerWithTimeInterval:(0.6-(0.02*level)) target:self selector:@selector(highliteNewButton)userInfo:nil repeats:YES];
+    countdownTimer=[NSTimer scheduledTimerWithTimeInterval:(1.0) target:self selector:@selector(enumerateLabel)userInfo:nil repeats:YES];
+    [button setUserInteractionEnabled:NO];
+}
+
+-(void)enumerateLabel{
+    secondesLeft--;
+
+    if (secondesLeft==0) {
+        [countdownTimer invalidate];
+        [timer invalidate];
+        [self littleBreak];
+        
+    }
+    self.timeLabel.text= [NSString stringWithFormat:@"%i", secondesLeft];
+}
+-(void)endGame{
+    [levelLabel setHidden:NO];
+
+    if (score>10) {
+        levelLabel.text= [NSString stringWithFormat:@"Level %i - Successfull!", level];
+        level++;
+
+    } else {
+        
+        levelLabel.text= [NSString stringWithFormat:@"Level: %i - Failed!", level];
+    }
+    secondesLeft=10;
+
+    /*hide Labels*/
+    [self.pointsLabel setHidden:YES];
+    [self.timeLabel setHidden:YES];
+    int width = self.view.frame.size.width/2 - 150/2;
+    scoreLabel1 = [[UILabel alloc]initWithFrame:CGRectMake(width, 250, 150, 40)];
+    scoreLabel1.textColor=[UIColor whiteColor];
+    scoreLabel1.textAlignment=NSTextAlignmentCenter;
+    scoreLabel2.font=[UIFont fontWithName:Nil size:20];
+
+    scoreLabel1.text= [NSString stringWithFormat:@"Your Score is"];
+    [self.view addSubview:scoreLabel1];
+
+    scoreLabel2 = [[UILabel alloc]initWithFrame:CGRectMake(width, 300, 150, 40)];
+    scoreLabel2.textColor=[UIColor whiteColor];
+    scoreLabel2.textAlignment=NSTextAlignmentCenter;
+    scoreLabel2.font=[UIFont fontWithName:Nil size:40];
+    scoreLabel2.text= [NSString stringWithFormat:@"%i", score];
+    [self.view addSubview:scoreLabel2];
+    
+    [self.startButton setUserInteractionEnabled:YES];
+    self.startButton.backgroundColor=[UIColor colorWithRed:1 green:106/255.0f blue:62/255.0f alpha:1];
+    self.startButton.alpha=1;
+    [timer invalidate];
+    
+}
+-(void)littleBreak{
+    [self.view viewWithTag:tagToDelete].backgroundColor=[UIColor colorWithRed:0/255.0f green:181/255.0f blue:227/255.0f alpha:1];
+    tagToDelete++;
+    NSTimer *littleBreak= [NSTimer scheduledTimerWithTimeInterval:(0.02) target:self selector:@selector(littleBreak2)userInfo:nil repeats:NO];
+    if (tagToDelete==36) {
+        NSTimer *endGame= [NSTimer scheduledTimerWithTimeInterval:(0.02) target:self selector:@selector(endGame)userInfo:nil repeats:NO];
+        [littleBreak invalidate];
+    }
+    }
+-(void)littleBreak2{
+    
+    [self.view viewWithTag:tagToDelete].backgroundColor=[UIColor colorWithRed:0/255.0f green:181/255.0f blue:227/255.0f alpha:1];
+    tagToDelete++;
+    NSTimer *littleBreak= [NSTimer scheduledTimerWithTimeInterval:(0.02) target:self selector:@selector(littleBreak)userInfo:nil repeats:NO];
+    if (tagToDelete==36) {
+        NSTimer *endGame= [NSTimer scheduledTimerWithTimeInterval:(0.02) target:self selector:@selector(endGame)userInfo:nil repeats:NO];
+        [littleBreak invalidate];
+    }
+    
+}
+- (IBAction)buttonPressed:(id)sender {
+    
+    UIButton *button=sender;
+    if (buttonTagToPress==button.tag) {
+        score +=1;
+        [self updateScoreLabel];
+    }
+
+}
 @end
